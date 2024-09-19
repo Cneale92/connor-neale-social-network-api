7f39c63e5bfeea14b11c0b686f8f1f86c6842803
Cneale92 <connorneale92@gmail.com>
2024-09-19 18:39:57 -0500

    commented on connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
-const mongoose = require('mongoose');
+// Import the connect function and connection object from the mongoose module
+const { connect, connection } = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialmedia');
+// Establish a connection to MongoDB using the local IP address and the default port, targeting the 'socialMediaAPI' database.
+connect("mongodb://127.0.0.1:27017/socialMediaAPI");
 
-module.exports = mongoose.connection;
+// Export the connection object for use in other parts of the application.
+module.exports = connection;
