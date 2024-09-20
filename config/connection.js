61ad1e8d29280d034d1ff07f4049c8704d0ea157
Cneale92 <connorneale92@gmail.com>
2024-09-19 19:52:52 -0500

    deleted starter code which wasn't actually starter code

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-// Import the connect function and connection object from the mongoose module
-const { connect, connection } = require("mongoose");
-
-// Establish a connection to MongoDB using the local IP address and the default port, targeting the 'socialMediaAPI' database.
-connect("mongodb://127.0.0.1:27017/socialMediaAPI");
-
-// Export the connection object for use in other parts of the application.
-module.exports = connection;
